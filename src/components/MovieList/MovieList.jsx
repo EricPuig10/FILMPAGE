9c5200a36e7763513c1d2468b0e0f25ff7044d18
EricPuig10 <puigvendrelloetona@gmail.com>
2022-05-24 15:27:28 +0200

    movielist passat a funcional, un error en el .map is not a function al fer edit

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,129 +1,248 @@
-import { Component } from "react";
+import React, { useEffect, useState } from "react";
+import { movieServices } from "../../services/movieServices";
 import { MovieCard } from "../MovieCard/MovieCard";
 import { MovieForm } from "../MovieForm/MovieForm";
-import { movieServices } from "../../services/movieServices";
 
-export class MovieList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      movies: [],
-      showForm: false,
-      movieToEdit: { title: "", id: "", imgUrl: "", yearOfProd: "" },
-      isEditMode: false,
-    };
-  }
+export const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+  const [isShowForm, setIsShowForm] = useState(false);
+  const [movieToEdit, setMovieToEdit] = useState({
+    title: "",
+    id: "",
+    imgUrl: "",
+    yearOfProd: "",
+  });
+  const [isEditMode, setIsEditMode] = useState(false);
 
-  componentDidMount = () => {
-    this.getAllMovies();
-  };
+  //canviem componentdidmount x useeffect
+  useEffect(() => {
+    getAllMovies();
+  }, []);
 
-  getAllMovies = () => {
+  const getAllMovies = () => {
     movieServices.getAllMovies().then((res) => {
-      this.setState({ movies: res });
+      setMovies(res);
     });
   };
 
-  showForm = () => {
-    if (this.state.showForm) this.setState({ showForm: false });
-    else this.setState({ showForm: true });
-    this.resetInputsForm();
+  const showForm = () => {
+    if (isShowForm) setIsShowForm(false);
+    else setIsShowForm(true);
+    resetInputsForm();
   };
 
-  addNewMovie = (data) => {
+  const addNewMovie = (data) => {
     // data.id = createUuid();
     // this.setState({movies:[...this.state.movies, data]});
     movieServices.addMovie(data).then((res) => {
-      this.setState({ movies: [...this.state.movies, res] });
+      setMovies([...movies, res]);
     });
-    this.setState({ showForm: false });
+    setIsShowForm(false);
   };
 
-  deleteMovie = (id) => {
-    let movieToDelete = this.state.movies.filter((movie) => movie.id === id);
+  const deleteMovie = (id) => {
+    let movieToDelete = movies.filter((movie) => movie.id === id);
     let deleteConfirmed = window.confirm(
       `Really remove ${movieToDelete[0].title} from the list?`
     );
     if (!deleteConfirmed) return;
-    let filterMovies = this.state.movies.filter((movie) => movie.id !== id);
+    let filterMovies = movies.filter((movie) => movie.id !== id);
     console.log(filterMovies);
 
     movieServices.deleteMovie(parseInt(id)).then((res) => {
-      if (res.id === id) this.setState({ movies: filterMovies });
+      if (res.id === id) setMovies(filterMovies);
     });
   };
 
-  editMovie = (id) => {
-    this.showForm();
-    let movieToEdit = this.state.movies.find((movie) => movie.id === id);
-    this.setState({ movieToEdit });
-    this.setState({ isEditMode: true });
+  const editMovie = (id) => {
+    showForm();
+    let movieToEdit = movies.find((movie) => movie.id === id);
+    setMovieToEdit(movieToEdit);
+    setIsEditMode(true);
   };
 
-  updateMovie = (newMovie) => {
-    let newMoviesState = this.state.movies;
+  const updateMovie = (newMovie) => {
+    let newMoviesState = movies;
     let movieToEditIndex = newMoviesState.findIndex(
       (movie) => movie.id === newMovie.id
     );
 
     movieServices.updateMovie(newMovie.id, newMovie).then((res) => {
       newMoviesState[movieToEditIndex] = res;
-      this.setState({ movies: newMoviesState });
+      setMovies({ newMoviesState });
     });
 
-    this.showForm();
+    showForm();
   };
 
-  resetInputsForm = (e) => {
-    this.setState({
-      newMovie: { id: "", title: "", yearOfProd: "", imgUrl: "", fav: "" },
-    });
+  const resetInputsForm = (e) => {
+    setMovieToEdit({ id: "", title: "", yearOfProd: "", imgUrl: "" });
   };
-  render() {
-    console.log(this.state.movies);
-    return (
-      <section>
-        {this.state.showForm ? (
-          <button
-            type="button"
-            onClick={this.showForm}
-            className="addMovieTitle"
-          >
-            {" "}
-            Back <i className="fa-solid fa-caret-up"></i>{" "}
-          </button>
-        ) : (
-          <button
-            type="button"
-            onClick={this.showForm}
-            className="addMovieTitle"
-          >
-            Add Movie <i className="fa-solid fa-caret-down"></i>
-          </button>
-        )}
-        {this.state.showForm ? (
-          <MovieForm
-            addNewMovie={this.addNewMovie}
-            movieToEdit={this.state.movieToEdit}
-            updateMovie={this.updateMovie}
-            isEditMode={this.state.isEditMode}
+
+  return (
+    <section>
+      {isShowForm ? (
+        <button type="button" onClick={showForm} className="addMovieTitle">
+          {" "}
+          Back <i className="fa-solid fa-caret-up"></i>{" "}
+        </button>
+      ) : (
+        <button type="button" onClick={showForm} className="addMovieTitle">
+          Add Movie <i className="fa-solid fa-caret-down"></i>
+        </button>
+      )}
+      {isShowForm ? (
+        <MovieForm
+          addNewMovie={addNewMovie}
+          movieToEdit={movieToEdit}
+          updateMovie={updateMovie}
+          isEditMode={isEditMode}
+        />
+      ) : (
+        ""
+      )}
+
+      <div id="containerListFilms">
+        {movies.map((movie, key) => (
+          <MovieCard
+            to={`/movie-info/${movie.id}`}
+            key={key}
+            movie={movie}
+            deleteMovie={deleteMovie}
+            editMovie={editMovie}
           />
-        ) : (
-          ""
-        )}
+        ))}
+      </div>
+    </section>
+  );
+};
 
-        <div id="containerListFilms">
-          {this.state.movies.map((movie, key) => (
-            <MovieCard
-              to={`/movie-info/${movie.id}`}
-              key={key}
-              movie={movie}
-              deleteMovie={this.deleteMovie}
-              editMovie={this.editMovie}
-            />
-          ))}
-        </div>
-      </section>
-    );
-  }
-}
+// import { Component } from "react";
+// import { MovieCard } from "../MovieCard/MovieCard";
+// import { MovieForm } from "../MovieForm/MovieForm";
+// import { movieServices } from "../../services/movieServices";
+
+// export class MovieList extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       movies: [],
+//       showForm: false,
+//       movieToEdit: { title: "", id: "", imgUrl: "", yearOfProd: "" },
+//       isEditMode: false,
+//     };
+//   }
+
+//   componentDidMount = () => {
+//     this.getAllMovies();
+//   };
+
+//   getAllMovies = () => {
+//     movieServices.getAllMovies().then((res) => {
+//       this.setState({ movies: res });
+//     });
+//   };
+
+//   showForm = () => {
+//     if (this.state.showForm) this.setState({ showForm: false });
+//     else this.setState({ showForm: true });
+//     this.resetInputsForm();
+//   };
+
+//   addNewMovie = (data) => {
+//     // data.id = createUuid();
+//     // this.setState({movies:[...this.state.movies, data]});
+//     movieServices.addMovie(data).then((res) => {
+//       this.setState({ movies: [...this.state.movies, res] });
+//     });
+//     this.setState({ showForm: false });
+//   };
+
+//   deleteMovie = (id) => {
+//     let movieToDelete = this.state.movies.filter((movie) => movie.id === id);
+//     let deleteConfirmed = window.confirm(
+//       `Really remove ${movieToDelete[0].title} from the list?`
+//     );
+//     if (!deleteConfirmed) return;
+//     let filterMovies = this.state.movies.filter((movie) => movie.id !== id);
+//     console.log(filterMovies);
+
+//     movieServices.deleteMovie(parseInt(id)).then((res) => {
+//       if (res.id === id) this.setState({ movies: filterMovies });
+//     });
+//   };
+
+//   editMovie = (id) => {
+//     this.showForm();
+//     let movieToEdit = this.state.movies.find((movie) => movie.id === id);
+//     this.setState({ movieToEdit });
+//     this.setState({ isEditMode: true });
+//   };
+
+//   updateMovie = (newMovie) => {
+//     let newMoviesState = this.state.movies;
+//     let movieToEditIndex = newMoviesState.findIndex(
+//       (movie) => movie.id === newMovie.id
+//     );
+
+//     movieServices.updateMovie(newMovie.id, newMovie).then((res) => {
+//       newMoviesState[movieToEditIndex] = res;
+//       this.setState({ movies: newMoviesState });
+//     });
+
+//     this.showForm();
+//   };
+
+//   resetInputsForm = (e) => {
+//     this.setState({
+//       newMovie: { id: "", title: "", yearOfProd: "", imgUrl: "", fav: "" },
+//     });
+//   };
+//   render() {
+//     console.log(this.state.movies);
+//     return (
+//       <section>
+//         {this.state.showForm ? (
+//           <button
+//             type="button"
+//             onClick={this.showForm}
+//             className="addMovieTitle"
+//           >
+//             {" "}
+//             Back <i className="fa-solid fa-caret-up"></i>{" "}
+//           </button>
+//         ) : (
+//           <button
+//             type="button"
+//             onClick={this.showForm}
+//             className="addMovieTitle"
+//           >
+//             Add Movie <i className="fa-solid fa-caret-down"></i>
+//           </button>
+//         )}
+//         {this.state.showForm ? (
+//           <MovieForm
+//             addNewMovie={this.addNewMovie}
+//             movieToEdit={this.state.movieToEdit}
+//             updateMovie={this.updateMovie}
+//             isEditMode={this.state.isEditMode}
+//           />
+//         ) : (
+//           ""
+//         )}
+
+//         <div id="containerListFilms">
+//           {this.state.movies.map((movie, key) => (
+//             <MovieCard
+//               to={`/movie-info/${movie.id}`}
+//               key={key}
+//               movie={movie}
+//               deleteMovie={this.deleteMovie}
+//               editMovie={this.editMovie}
+//             />
+//           ))}
+//         </div>
+//       </section>
+//     );
+//   }
+// }
