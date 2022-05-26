f75deea8bb832ad85ad8d0ba66e1ec273bf9f095
EricPuig10 <puigvendrelloetona@gmail.com>
2022-05-26 10:26:06 +0200

    router canviat a basename

--- src/application/Router.jsx
+++ src/application/Router.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home } from '../pages/Home';
-import { InfoFilm } from '../pages/InfoFilm';
-import { LogIn } from '../pages/LogIn';
-
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { InfoFilm } from "../pages/InfoFilm";
+import { LogIn } from "../pages/LogIn";
 
 export default function Router(props) {
-    let to = props.to
+  let to = props.to;
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/FILMPAGE" element={<Home/>}/>
-            <Route path="/LogIn" element={<LogIn/>}/>
-            <Route path="/movie-info/:id" element={<InfoFilm/>}/>
-            
-        </Routes>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/LogIn" element={<LogIn />} />
+        <Route path="/movie-info/:id" element={<InfoFilm />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
